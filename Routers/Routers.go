0a4fcab477da5e50b9b8a7d0af4d01f4a53b53af
learnerous <oussama.benbrahim@etudiant-enit.utm.tn>
2024-02-05 14:27:04 +0100

    f

--- Routers/Routers.go
+++ Routers/Routers.go
@@ -2,11 +2,19 @@ package routers
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/learnerous/mini-projet/controllers"
 )
 
 // func helloDataProvider(c *gin.Context) {
 // 	// Implement the logic for helloDataProvider here
 // }
+
+package routers
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/path/to/controllers" // Import the controllers package
+)
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
